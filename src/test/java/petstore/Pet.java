e3cae1b78abf4f8f3e93837ec2eec9001405a1cb
Thailamari <thailamari@yahoo.com.br>
2021-10-22 16:51:03 -0300

    Completei o script excluirPet(delete)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -45,10 +45,69 @@ public class Pet {
                 .statusCode(200)
                 .body("name", is("Atena"))
                 .body("status", is("available"))
-                .body("category.name", is("AX2345LORT"))
+                .body("category.name", is("dog"))
                 .body("tags.name", contains("data"))
         ;
 
+    }
+    @Test(priority=2)
+    public void consultarPet(){
+        String petId = "100";
+
+        String token =
+                given()
+                        .contentType("application/json")
+                        .log().all()
+                        .when()
+                        .get(uri + "/" + petId)
+                        .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("name", is("Atena"))
+                        .body("category.name", is("dog"))
+                        .body("status",is("available"))
+                        .extract()
+                        .path("category.name")
+                ;
+        System.out.println("O token é " + token);
+
+    }
+
+    @Test(priority=3)
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson("Data base/pet2.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+                .when()
+                .put(uri)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Atena"))
+                .body("status",is("sold"))
+        ;
+    }
+
+    @Test (priority = 4)
+    public void excluirPet(){
+        String petId = "100";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .when()
+                .delete(uri + "/" + petId)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is(petId))
+
+        ;
     }
 
 }
